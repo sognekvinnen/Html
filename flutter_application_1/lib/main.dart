55d5e59e087b1d18c09facd361b7e71db1506f43
sognekvinnen <157130429+sognekvinnen@users.noreply.github.com>
2025-10-23 07:03:40 +0000

    a

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -12,7 +12,7 @@ class MainApp extends StatelessWidget {
     return const MaterialApp(
       home: Scaffold(
         body: Column(children: [
-          Text('Hello World!'),
+          Text('Hello World!!'),
         ]),
       ),
     );
